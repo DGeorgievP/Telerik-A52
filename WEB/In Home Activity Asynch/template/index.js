708d42da0c7be931f07b80c1faf8b39bf823c4e5
DGeorgievP <dimitargeorgievpavlov@gmail.com>
2023-10-04 23:15:34 +0300

    asynch in home , done

--- WEB/In Home Activity Asynch/template/index.js
+++ WEB/In Home Activity Asynch/template/index.js
@@ -1,1 +1,41 @@
 import fs from 'fs';
+
+fs.readFile('data/ids-to-names.json', { encoding: 'utf-8' }, function (err, data) {
+    if (err) {
+        console.error(err);
+        return;
+    };
+
+    const studentNames = JSON.parse(data)
+
+    fs.readFile('data/ids-to-cities.json', { encoding: 'utf-8' }, function (err, data) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        const studentCities = JSON.parse(data);
+
+        fs.readFile('data/ids-to-grades.json', { encoding: 'utf-8' }, function (err, data) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            const studentsGrades = JSON.parse(data);
+
+            const studentRecords = studentNames
+                .map(el => ({...el, city: studentCities.find(c => c.id === el.id).city }))
+                .map(el => ({...el, grades: studentsGrades.find(g => g.id === el.id).grades}))
+
+            fs.writeFile('data/student-data.json', JSON.stringify(studentRecords), { encoding: 'utf-8' }, function (err) {
+                if (err) {
+                    console.error(err)
+                    return;
+                } else {
+                    console.log('Students records are live')
+                }
+            })
+        });
+    });
+});
