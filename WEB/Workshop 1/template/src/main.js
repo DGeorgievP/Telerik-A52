cc33b387286f53b38d9692c802b27abce22df9ca
DGeorgievP <dimitargeorgievpavlov@gmail.com>
2023-10-05 14:37:10 +0300

    MainJS done and working

--- WEB/Workshop 1/template/src/main.js
+++ WEB/Workshop 1/template/src/main.js
@@ -7,8 +7,8 @@ new Promise(function (resolve, reject) {
     reader.read('data/tasks.all.json', 'utf-8')
     .then((data) => JSON.parse(data))
         .then((data) => {
-            const pendingTasks = data.tasks.filter((task) => !task.done)
-            const doneTasks = data.tasks.filter((task) => task.done)
+            const pendingTasks = data.tasks.filter((task) => task.status === 'pending')
+            const doneTasks = data.tasks.filter((task) => task.status === 'done')
 
             const writePending = reader.write('data/tasks.pending.json', JSON.stringify(pendingTasks))
             const writeDone = reader.write('data/tasks.done.json', JSON.stringify(doneTasks))
