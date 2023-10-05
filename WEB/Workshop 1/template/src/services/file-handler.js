f3a60aaca22231140bb12ba505c13e2550e900f6
DGeorgievP <dimitargeorgievpavlov@gmail.com>
2023-10-05 11:31:20 +0300

    read method done

--- WEB/Workshop 1/template/src/services/file-handler.js
+++ WEB/Workshop 1/template/src/services/file-handler.js
@@ -1,5 +1,6 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-shadow */
+import { rejects } from 'assert';
 import fs from 'fs';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
@@ -12,12 +13,24 @@ export class FileHandler {
   read(filename = '') {
     filename = join(__dirname, '..', filename);
 
-    // missing implementation
+    return new Promise((resolve, reject) => {
+      
+      fs.readFile('data/tasks.all.json', { encoding: 'utf-8' }, function (err, data) {
+
+        if (err) {
+          reject(new Error(`Error reading file`))
+        } else {
+          resolve(data)
+        }
+
+      })
+    })
   }
+
 
   write(filename = '', data = '') {
     filename = join(__dirname, '..', filename);
 
-    // missing implementation
+    
   }
 }
