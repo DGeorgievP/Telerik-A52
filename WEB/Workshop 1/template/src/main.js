9efbb0bc8d56cad6ed177ff86efb770732bd15a8
DGeorgievP <dimitargeorgievpavlov@gmail.com>
2023-10-05 14:29:19 +0300

    Fixes to the mainJS

--- WEB/Workshop 1/template/src/main.js
+++ WEB/Workshop 1/template/src/main.js
@@ -5,12 +5,13 @@ const reader = new FileHandler();
 new Promise(function (resolve, reject) {
 
     reader.read('data/tasks.all.json', 'utf-8')
+    .then((data) => JSON.parse(data))
         .then((data) => {
-            const pendingTasks = data.tasks.status.filter((task) => !task.done)
-            const doneTasks = data.tasks.status.filter((task) => task.done)
+            const pendingTasks = data.tasks.filter((task) => !task.done)
+            const doneTasks = data.tasks.filter((task) => task.done)
 
-            const writePending = reader.write(`data/tasks.pending.json`, JSON.stringify(pendingTasks))
-            const writeDone = reader.write(`data/tasks.done.json`, JSON.stringify(doneTasks))
+            const writePending = reader.write('data/tasks.pending.json', JSON.stringify(pendingTasks))
+            const writeDone = reader.write('data/tasks.done.json', JSON.stringify(doneTasks))
 
             return Promise.all([writePending, writeDone])
         })
