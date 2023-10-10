f99a23277fb49317ddc1efb94b56e04d17a8a0a0
DGeorgievP <dimitargeorgievpavlov@gmail.com>
2023-10-10 08:55:51 +0300

    done

--- WEB/In Home Activity Asynch-Await/template/index.js
+++ WEB/In Home Activity Asynch-Await/template/index.js
@@ -1,1 +1,17 @@
 import fs from 'fs/promises';
+
+ (async () => {
+    const data = await Promise.all([
+        fs.readFile('/data/ids-to-names.json', { encoding: 'utf-8' }),
+        fs.readFile('/data/ids-to-cities.json', { encoding: 'utf-8' }),
+        fs.readFile('/data/ids-to-grades.json', { encoding: 'utf-8' }),
+    ])
+
+    const [names, cities, grades] = data.map(x => JSON.parse(x))
+
+    const studentRecords = names
+    .map(el => ({...el, city: cities.find(c => c.id === el.id).city }))
+    .map(el => ({...el, grades: grades.find(g => g.id === el.id).grades}))
+
+fs.writeFile('data/student-data.json', JSON.stringify(studentRecords), { encoding: 'utf-8' })
+})
